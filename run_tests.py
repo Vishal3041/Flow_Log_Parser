046a7886e42bb1b33cc13403665ac33c45c13eab
Vishal Rajeshkumar Tripathi <vishaltripathi1497@gmail.com>
2024-09-23 22:38:20 -0700

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -9,7 +9,7 @@ def run_test(test_dir):
     expected_output = os.path.join(test_dir, 'expected_output')
     actual_output = os.path.join(test_dir, 'output')
 
-    # Run the flow_logs_parser.py script with the test files
+    # run the flow_logs_parser.py with the required files
     try:
         subprocess.run(['python3', 'flow_logs_parser.py', lookup_table, flow_logs, actual_output], check=True)
     except subprocess.CalledProcessError as e:
@@ -17,7 +17,7 @@ def run_test(test_dir):
         print(f"Command failed with error: {e}")
         return
 
-    # Compare actual output to expected output
+    # open both the excepted and output files and compare the result
     with open(expected_output, 'r') as expected_file, open(actual_output, 'r') as actual_file:
         expected_data = expected_file.read().strip()
         actual_data = actual_file.read().strip()
@@ -33,10 +33,10 @@ def run_test(test_dir):
 
 
 if __name__ == "__main__":
-    # Define your test cases directories
+    # list all the directories where test_cases are created
     test_cases = ['test_cases/test1', 'test_cases/test2', 'test_cases/test3', 'test_cases/test4',
                     'test_cases/test6', 'test_cases/test7', 'test_cases/test5']
 
-    # Run tests
+    # Run all the test_cases one-by-one included in the list above
     for test_case in test_cases:
         run_test(test_case)
